798fa1d39cb60095d8a2a7bb22418eb7138fa6f6
lhrmin <lhrmin64744@163.com>
2022-04-20 22:01:59 +0800

    Create demo.js

---
+++ project/static/js/demo.js
@@ -0,0 +1,19 @@
+$( ".settings-button" ).click(function() {
+  $( ".site-wrapper" ).toggleClass( "site-wrapper-wide" );
+});
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	
+});
